cca3842b45c1217c23964577179924e77daae54e
sumaiya-mugdha <mugdhakhan720@gmail.com>
2026-02-15 10:59:17 +0600

    new practice 2

---
+++ Control Flow/string.py
@@ -0,0 +1,16 @@
+str1= "i am a new string who find a thing"
+print(str1)
+print(str1.endswith("ing"))
+print(str1.capitalize())
+print(str1.replace("thing","materials"))
+print(str1.find("thing"))
+print(str1.count("a"))
+
+#tasks
+name=input("Enter your name:")
+print(len(name))
+print(name.capitalize())
+print(name.find("mugdha"))
+
+# reverse
+print(name[::-1])
