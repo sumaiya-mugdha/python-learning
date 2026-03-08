5f6bcca2f6aecbbc6753ed443726f2c022162673
sumaiya-mugdha <mugdhakhan720@gmail.com>
2026-03-08 10:18:25 +0600

    function

--- Control Flow/function.py
+++ Control Flow/function.py
@@ -10,7 +10,9 @@ def factorial(n):
     if n==0:
         return 1
     else:
+        print(n)    
         return n*factorial(n-1)
+    print(factorial(n))
 
 factorial(int(input("Enter a number: ")))
 
